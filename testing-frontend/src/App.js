e22e76640c06e4a7cfc96887e1af842db5c9c499
meghana-41 <meghana.chakravarthi@think41.com>
2025-04-14 16:59:59 +0530

    Update App.js with new welcome message

--- testing-frontend/src/App.js
+++ testing-frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World from Testing Branch!</h1>
-        <p>This is testingbranch1</p>
+        <h1>Welcome to My App!</h1>
+        <p>This is a different version of the header content.</p>
       </header>
     </div>
   );
